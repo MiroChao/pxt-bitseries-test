90047dec93ff40051a6ac0dbf71cded76fe8e6ec
MiroChao <531414952@qq.com>
2019-02-22 18:17:15 +0800

    Updates.

--- main.ts
+++ main.ts
@@ -149,7 +149,8 @@ namespace BitTest {
     //% grove.fieldOptions.width=200
     //% grove.fieldOptions.columns=3
     //% Unit.fieldEditor="gridpicker"
-    //% Unit.fieldOptions.width=200
+    //% Unit.fieldOptions.width=70
+    //% Unit.fieldOptions.columns=1
     //% group="Grove Modules"
     //% weight=100
     export function measureInCentimeters(grove: GrovePort, Unit: DistanceUnit): number {
@@ -195,9 +196,9 @@ namespace BitTest {
     */
     //% blockId=read_Din_value
     //% block="digital read pin $grove"
-    //% Din.fieldEditor="gridpicker"
-    //% Din.fieldOptions.width=200
-    //% Din.fieldOptions.columns=3
+    //% grove.fieldEditor="gridpicker"
+    //% grove.fieldOptions.width=200
+    //% grove.fieldOptions.columns=3
     //% group="Digital"
     export function read_Din_value(grove: GrovePort): number {
         selectPins.grove = grove;
@@ -251,7 +252,7 @@ namespace BitTest {
     //% blockId=read_Ain
     //% block="analog read pin $analogIO"
     //% analogIO.fieldEditor="gridpicker"
-    //% analogIO.fieldOptions.width=200
+    //% analogIO.fieldOptions.width=50
     //% analogIO.fieldOptions.columns=3
     //% group="Analog"
     //% weight=50
@@ -267,8 +268,8 @@ namespace BitTest {
     //% blockId=convert_Ain
     //% block="map pin $analogIO|to low $low_value|high $high_value"
     //% analogIO.fieldEditor="gridpicker"
-    //% analogIO.fieldOptions.width=200
-    //% analogIO.fieldOptions.columns=3
+    //% analogIO.fieldOptions.width=70
+    //% analogIO.fieldOptions.columns=1
     //% group="Analog"
     //% weight=40
     export function convert_Ain(analogIO: AnalogPort, low_value: number, high_value: number): number {
@@ -281,10 +282,10 @@ namespace BitTest {
     * write value to the analog ports
     */
     //% blockId=write_analog
-    //% block="analog write pin $analogIO| to $Aout"
+    //% block="analog write pin $analogIO| to $value"
     //% analogIO.fieldEditor="gridpicker"
-    //% analogIO.fieldOptions.width=200
-    //% analogIO.fieldOptions.columns=3
+    //% analogIO.fieldOptions.width=70
+    //% analogIO.fieldOptions.columns=1
     //% value.min=0 value.max=1023
     //% value.defl=1023
     //% group="Analog"
@@ -301,8 +302,8 @@ namespace BitTest {
     //% blockId=config_PWM
     //% block="analog set period pin $analogIO|(PWM) to (us) $PWMvalue"
     //% analogIO.fieldEditor="gridpicker"
-    //% analogIO.fieldOptions.width=200
-    //% analogIO.fieldOptions.columns=3
+    //% analogIO.fieldOptions.width=70
+    //% analogIO.fieldOptions.columns=1
     //% PWMvalue.defl=20000
     //% group="Analog"
     //% weight=20
